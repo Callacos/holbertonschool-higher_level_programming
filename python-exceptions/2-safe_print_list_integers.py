23d92ca0c3ed7a313053f799ae4a582bb9b5d2ef
Callacos <callacos74@gmail.com>
2025-01-24 16:27:05 +0100

    Refactor safe_print_list_integers to improve integer type checking and handle IndexError more gracefully

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            pass
-        except IndexError:
-            break
-    print()
+    try:
+        for i in range(x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+        print()
+    except IndexError:
+        pass
     return count
