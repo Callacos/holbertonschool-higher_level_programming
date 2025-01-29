2f8a56e2304866c0ed0e58ef92130d8f189e504c
Callacos <callacos74@gmail.com>
2025-01-29 13:35:29 +0100

    Add docstrings to Square class and module for improved clarity

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Module that defines the Square class."""
+
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size):
+        """Initializes a new Square.
+
+        Args:
+            size: The size of the square.
+        """
         self._size = size
