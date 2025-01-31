5982c1a21c6644f1b7d2dc8521b2428563405f8e
Callacos <callacos74@gmail.com>
2025-01-31 11:09:08 +0100

    Simplify Square class docstring and retain class definition

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
+"""Square class with a private instance"""
+
+
 class Square:
-    """
-    Classe représentant un carré.
-
-    Cette classe est actuellement vide et sert de base pour définir
-    un carré. Elle peut être étendue pour inclure des attributs et 
-    des méthodes spécifiques aux propriétés d'un carré.
-
-    Attributs:
-        Aucun pour le moment.
-
-    Méthodes:
-        Aucune pour le moment.
-    """
+    """Square class with a private instance"""
     pass
