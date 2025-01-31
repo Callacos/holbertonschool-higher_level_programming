974b61074d680254e99b7f4178f5bd2ad7cceab2
Callacos <callacos74@gmail.com>
2025-01-31 14:14:15 +0100

    Fix string representation method to use symbol parameter for printing rectangle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -56,7 +56,7 @@ class Rectangle:
         """Retourne une représentation du rectangle avec print_symbol."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
+            return "\n".join([symbol * self.__width] * self.__height)
 
     def __repr__(self):
         """Retourne une représentation string de l'objet rectangle."""
