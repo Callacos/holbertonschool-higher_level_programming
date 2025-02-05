18a0e2cc76e563d6708e10229bb6ee8e83a530c2
Callacos <callacos74@gmail.com>
2025-02-05 18:07:16 +0100

    Fix formatting of docstring in area method of BaseGeometry class

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,6 +6,6 @@ class BaseGeometry:
     """ Classe BaseGeometry """
 
     def area(self):
-        """ Méthode qui soulève une exception avec un message 
+        """ Méthode qui soulève une exception avec un message
         indiquant que l'area() n'est pas implémentée. """
         raise Exception("area() is not implemented")
