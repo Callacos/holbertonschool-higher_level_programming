7d6b8e7f97c1eb283a19a827bb3282e0ec4e7a2b
Callacos <callacos74@gmail.com>
2025-02-05 16:32:19 +0100

    Add French docstring to lookup function for attribute inspection

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """Retourne la liste des attributs et méthodes disponibles d'un objet.
+
+    Args:
+        obj: L'objet à inspecter
+
+    Returns:
+        Liste des attributs et méthodes de l'objet
+    """
     return dir(obj)
