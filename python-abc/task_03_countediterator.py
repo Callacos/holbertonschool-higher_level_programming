a1964e168751df4e9cf5312d2a08f14f36d68dc2
Callacos <callacos74@gmail.com>
2025-02-07 17:26:55 +0100

    Refactor CountedIterator to use English messages and improve iteration logic

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-class CountedIterator:
-    """Un itérateur qui compte le nombre d'éléments parcourus."""
 
-    def __init__(self, iterable):
-        """Initialise l'itérateur compté avec un itérable."""
-        self.iterator = iter(iterable)
-        self.count = 0
+class CountedIterator:
+    """Iterator that counts the number of items iterated over."""
+
+    def __init__(self, data):
+        self._data = data
+        self._index = 0
+        self._count = 0
 
     def __iter__(self):
-        """Retourne l'objet lui-même comme itérateur."""
         return self
 
     def __next__(self):
-        """Retourne le prochain élément et incrémente le compteur."""
-        self.count += 1
-        return next(self.iterator)
+        if self._index >= len(self._data):
+            raise StopIteration
+        item = self._data[self._index]
+        self._index += 1
+        self._count += 1
+        return item
 
     def get_count(self):
-        """Retourne le nombre d'éléments parcourus."""
-        return self.count
+        """Return the number of items iterated over."""
+        return self._count
 
+# Script demonstrating the use of CountedIterator
+data = [1, 2, 3, 4]
+counted_iter = CountedIterator(data)
 
-# Test de la classe CountedIterator
-if __name__ == "__main__":
-    # Création d'un CountedIterator avec une liste
-    counted_iter = CountedIterator([1, 2, 3, 4, 5])
-
-    # Itération sur les éléments
-    for item in counted_iter:
-        print(f"Élément: {item}, Compteur: {counted_iter.get_count()}")
-
-    # Vérification du compteur final
-    print(f"Nombre total d'éléments parcourus: {counted_iter.get_count()}")
-
-    # Test avec un appel manuel à __next__
-    counted_iter2 = CountedIterator("abc")
-    print(next(counted_iter2))
-    print(next(counted_iter2))
-    print(f"Compteur après 2 appels manuels: {counted_iter2.get_count()}")
+try:
+    while True:
+        item = next(counted_iter)
+        print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
+except StopIteration:
+    print("No more items.")
