deeefd04c966b4721a450651132902ae5e83d6bd
Callacos <callacos74@gmail.com>
2025-02-12 15:46:08 +0100

    Refactor read_file function docstring for clarity and consistency

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
+"""Module pour lire un fichier texte (UTF8) et afficher son contenu sur stdout."""
+
+
 def read_file(filename=""):
-    """Lit un fichier texte (UTF8) et affiche son contenu sur stdout.
-
-    Args:
-        filename (str): Le nom du fichier à lire. Par défaut, une chaîne vide.
-
-    Comportement:
-        Ouvre le fichier en mode lecture avec encodage UTF-8
-    """
+    """Lit un fichier texte (UTF8) et affiche son contenu sur stdout """
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
