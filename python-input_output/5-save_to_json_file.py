bfc7f713dd72347adb534f67bae6b4142e8ee9d5
Callacos <callacos74@gmail.com>
2025-02-14 11:31:03 +0100

    Refactor save_to_json_file function to improve JSON writing and add module docstring

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+"""Module for saving objects to JSON files."""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-
-    """
-    Writes an object to a text file using a JSON representation.
-    """
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w", encoding="utf-8") as f:
+        obj = json.dumps(my_obj)
+        f.write(obj)
