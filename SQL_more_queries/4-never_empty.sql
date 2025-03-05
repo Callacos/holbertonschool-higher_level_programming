da40d50d6bae5b7c7160e25bfc63a8f0b6cc8a69
Callacos <callacos74@gmail.com>
2025-03-05 11:07:06 +0100

    Correct comment formatting for id_not_null table creation in 4-never_empty.sql

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
---create table id_not_null with id column that is not null
+-- Create table id_not_null with id column that is not null
 CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
