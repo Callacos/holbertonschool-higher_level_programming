63de74f15ebc085cb2e72cfe8a7516203f9ee6bc
Callacos <callacos74@gmail.com>
2025-03-05 10:36:41 +0100

    Update database name in 2-create_read_user.sql to hbtn_0d_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- créé la base de données hbtn_0c_0
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_2`;
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Accorde tous les privilèges à user_0d_1
