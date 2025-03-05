0aab24a337b98190e69bbdd4cef024dc56ebbdc9
Callacos <callacos74@gmail.com>
2025-03-05 08:40:32 +0100

    Fix insertion order and correct spelling in SQL script for database creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- This file contains the full creation of the database and the tables
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table(id, name, score) VALUES (4, 'Geaorge', 8);
-INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO second_table(id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
