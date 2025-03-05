f703ba304546242f3a11c0b174d4c34888962d26
Callacos <callacos74@gmail.com>
2025-03-05 10:39:31 +0100

    Fix database name in GRANT statement in 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Accorde tous les privilèges à user_0d_1
-GRANT SELECT ON hbtn_0c_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
