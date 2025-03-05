9107452155d7d8930143bf8a85f7c8e737573ad9
Callacos <callacos74@gmail.com>
2025-03-05 11:10:13 +0100

    Add SQL script to create unique_id table with unique id and name columns

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- create a table called unique_id with two columns: id and name. The id column should be unique and have a default value of 1.
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
