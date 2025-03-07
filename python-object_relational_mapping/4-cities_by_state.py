8f962490b0ff448f6e4c1ea7afde1d2944c191fa
Callacos <callacos74@gmail.com>
2025-03-07 14:48:09 +0100

    Add Python script and SQL setup for cities by state retrieval

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This 
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    try:
+        # Check if all arguments are provided
+        if len(sys.argv) != 4:
+            print("Usage: {} username password database".format(sys.argv[0]))
+            sys.exit(1)
+
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
