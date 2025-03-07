dc3b9feb5d79d76e76aedfca193bddfd8375dd22
Callacos <callacos74@gmail.com>
2025-03-07 14:24:48 +0100

    Refactor state filtering script to use parameterized queries and improve readability

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python3
-""""
-This is Elon MUSK
+"""
+This
 """
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    try:
-        if len(sys.argv) != 5:
-            print("Usage: {} username password database state_name".format(
-                sys.argv[0]))
-            sys.exit(1)
 
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-        )
-        cursor = db.cursor()
-        query = "SELECT * FROM states\
-            WHERE name = '{}'\
-            ORDER BY id ASC".format(sys.argv[4])
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    state_name = sys.argv[4]
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
 
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-        sys.exit(1)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
