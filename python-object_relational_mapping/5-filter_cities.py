25bc8f610925fb76be4a887ce327a9d3bf7d16be
Callacos <callacos74@gmail.com>
2025-03-07 19:52:39 +0100

    Add script to filter cities by state with error handling and usage validation

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    try:
+        # Check if all arguments are provided
+        if len(sys.argv) != 4:
+            print("Usage: {} username password database state name".format(sys.argv[0]))
+            sys.exit(1)
+
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+
+        cursor = db.cursor()
+        cursor.execute("""SELECT cities.name \
+                    FROM cities JOIN states \
+                    ON cities.state_id = states.id \
+                    WHERE CONVERT (states.name USING Latin1) \
+                    COLLATE Latin1_General_CS = %s \
+        ORDER BY c.id ASC""")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
