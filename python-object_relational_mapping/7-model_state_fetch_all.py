9cf656b5cce46862f20ad959c94a5da892f7e0f9
Callacos <callacos74@gmail.com>
2025-03-07 20:29:07 +0100

    Fix line length in usage message for better readability

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
