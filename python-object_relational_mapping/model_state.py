e031cdf008ab775a0c2604c5a1e2f6e4134bfc87
Callacos <callacos74@gmail.com>
2025-03-07 20:22:01 +0100

    Refactor model_state.py to define State class and improve metadata handling

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,18 +2,18 @@
 """
 This is the end, hold your breath and count to ten
 """
-import sys
-from model_state import Base, State
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import (create_engine)
+meta = MetaData()
+Base = declarative_base(metadata=meta)
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer,
+                autoincrement=True,
+                unique=True,
+                primary_key=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
