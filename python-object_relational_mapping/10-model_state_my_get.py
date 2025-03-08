a75ca9c85092dec0ca85ab71abc53034d2b5fab8
Callacos <callacos74@gmail.com>
2025-03-08 16:41:14 +0100

    Refactor script to streamline state retrieval and improve usage instructions

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -6,26 +6,20 @@ as argument from the database hbtn_0e_6_usa
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <mysql username> <mysql password> <database name> "
-              "<state name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database, state_name = sys.argv[1:5]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database))
-
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3],
+            pool_pre_ping=True
+        )
+    )
+    search_name = sys.argv[4]
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    state = session.query(State).filter(State.name == state_name).first()
-
-    if state:
-        print("{}".format(state.id))
-    else:
-        print("Not found")
-
-session.close()
+    result = session.query(State).filter(State.name == search_name).first()
+    print("Not found" if not result else result.id)
